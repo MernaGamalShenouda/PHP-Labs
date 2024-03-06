1b53652173848f8e4c7afadbc48fed26bf5b55f3
Merna Gamal <merna.gamal.shenouda@gmail.com>
2024-03-06 12:14:49 +0200

    Unique visits counter using OOP PHP

--- Lab3/index.php
+++ Lab3/index.php
@@ -19,36 +19,8 @@ $visitors = $visitorsCounter->getCounter();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <style>
-       body {
-            display: flex;
-            justify-content: center;
-            flex-direction: column;
-            align-items: center;
-            height: 100vh;
-            margin: 0;
-            font-family: Arial, sans-serif;
-            background-color: #f5f5f5; /* Add a background color */
-        }
-
-        div {
-            text-align: center;
-            background-color: #fff; /* White background color */
-            padding: 20px;
-            border-radius: 8px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); /* Add a subtle box shadow */
-        }
-
-        h2 {
-            font-weight: bold; /* Make the heading bold */
-        }
-
-        p {
-            font-size: 24px;
-            margin: 10px 0;
-            color: #333; /* Dark text color */
-        }
-    </style>
+    <link rel="stylesheet" href="style.css"> 
+     
 </head>
 <body>
 <div> Counted Unique Visitors</div> 
